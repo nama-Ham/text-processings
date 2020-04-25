fa1b282b5b47ca1a5327518b651c121e69f00ffc
nama-Ham <jparch.ron.1214@gmail.com>
2020-04-26 01:54:32 +0900

    code v0.0.1

--- weaken-japanese/src/main.py
+++ weaken-japanese/src/main.py
@@ -4,6 +4,52 @@ import sqlite3
 import re
 import random
 import numpy as np
+from collections import namedtuple
+
+
+Word = namedtuple('Word', 'wordid lang lemma pron pos')
+Sense = namedtuple('Sense', 'synset wordid lang rank lexid freq src')
+Synset = namedtuple('Synset', 'synset pos name src')
+#SynsetDef = namedtuple('SynsetDef', 'synset lang defi sid')
+conn = sqlite3.connect("../dict/synonym/wnjpn.db")
+
+def get_word(lemma):
+    cur = conn.execute("select * from word where lemma=?", (lemma,))
+    return [Word(*row) for row in cur]
+
+def get_senses(word, lang="jpn"):
+    cur = conn.execute("select * from sense where wordid=? and lang=?", (word.wordid, lang))
+    return [Sense(*row) for row in cur]
+
+def get_words_from_synset(synset, word, lang):
+    cur = conn.execute("select word.* from sense, word where synset=? and word.lang=? and sense.wordid=word.wordid and word.wordid<>?;", (synset, lang, word.wordid))
+    return [Word(*row) for row in cur]
+
+def search_synonyms(word):
+    wobj = get_word(word)
+    synonym_list = []
+    if wobj:
+        senses = get_senses(wobj[0])
+        for s in senses:
+            synonyms = get_words_from_synset(s.synset, wobj[0], "jpn")
+            for syn in synonyms:
+                if syn.lemma not in synonym_list:
+                    synonym_list.append(syn.lemma)
+    return synonym_list
+
+
+def choose_synonym(word):
+    synonym = word
+    pos, basic = analyze_pos(word)
+    synonyms = search_synonyms(word)
+    idxs = np.arange(0, len(synonyms), 1)
+    np.random.shuffle(idxs)
+    for idx in idxs:        
+        synonym_pos, synonym_basic = analyze_pos(synonyms[idx])
+        if synonym_pos == pos and synonym_basic == basic:
+            synonym = synonyms[idx]
+            break
+    return synonym
 
 
 def transform(basic, conjugate):
@@ -18,8 +64,7 @@ def transform(basic, conjugate):
         dict_file = "Adverb"
     elif pos == "名詞":
         dict_file = "Noun"
-    #elif pos == "助詞":
-    #    dict_file = "Postp"
+
 
     with open("../dict/pos/" + dict_file + ".csv", "rb") as f:
         verbs = f.readlines()
@@ -51,47 +96,6 @@ def analyze_pos(word):
     return p, kb
 
 
-def search_synonyms(word):
-    conn = sqlite3.connect("../dict/synonym/wnjpn.db")
-    cur = conn.execute("select wordid from word where lemma='%s'" % word)
-    word_id = -1
-    for row in cur:
-        word_id = row[0]
-
-    similar_words = []
-    cur = conn.execute("select synset from sense where wordid='%s'" % word_id)
-    synsets = []
-    for row in cur:
-        synsets.append(row[0])
-
-    # 概念に含まれる単語を検索
-    for synset in synsets:
-        cur3 = conn.execute("select wordid from sense where (synset='%s' and wordid!=%s)" % (synset,word_id))
-        for row3 in cur3:
-            target_word_id = row3[0]
-            cur3_1 = conn.execute("select lemma from word where wordid=%s" % target_word_id)
-            for row3_1 in cur3_1:
-            	# 全角文字のみでユニークな類義語のみ抽出
-                if re.match(r'[^\x01-\x7E]', row3_1[0]) and row3_1[0] not in similar_words:
-                    similar_words.append(row3_1[0])
-    return similar_words
-
-
-def choose_synonym(word):
-    synonym = word
-    pos, basic = analyze_pos(word)
-    synonyms = search_synonyms(word)
-    print(synonyms)
-    idxs = np.arange(0, len(synonyms), 1)
-    np.random.shuffle(idxs)
-    for idx in idxs:        
-        synonym_pos, synonym_basic = analyze_pos(synonyms[idx])
-        if synonym_pos == pos and synonym_basic == basic:
-            synonym = synonyms[idx]
-            break
-    return synonym
-
-
 def convert_kata_to_hira(katakana):
     hira_tupple = ('あ','い','う','え','お','か','き','く','け','こ','さ','し','す','せ','そ','た','ち','つ','て','と','な','に','ぬ','ね','の','は','ひ','ふ','へ','ほ','ま','み','む','め','も','や','ゆ','よ','ら','り','る','れ','ろ','わ','を','ん','っ','ゃ','ゅ','ょ','ー','が','ぎ','ぐ','げ','ご','ざ','じ','ず','ぜ','ぞ','だ','ぢ','づ','で','ど','ば','び','ぶ','べ','ぼ','ぱ','ぴ','ぷ','ぺ','ぽ')
     kata_tupple = ('ア','イ','ウ','エ','オ','カ','キ','ク','ケ','コ','サ','シ','ス','セ','ソ','タ','チ','ツ','テ','ト','ナ','ニ','ヌ','ネ','ノ','ハ','ヒ','フ','ヘ','ホ','マ','ミ','ム','メ','モ','ヤ','ユ','ヨ','ラ','リ','ル','レ','ロ','ワ','ヲ','ン','ッ','ャ','ュ','ョ','ー','ガ','ギ','グ','ゲ','ゴ','ザ','ジ','ズ','ゼ','ゾ','ダ','ヂ','ヅ','デ','ド','バ','ビ','ブ','ベ','ボ','パ','ピ','プ','ペ','ポ')
@@ -106,7 +110,7 @@ def convert_kata_to_hira(katakana):
 
 def mistake_ppp(ppp):
     substitute_ppp = ppp
-    mistake_set = np.array([["が", "は"],["から", "より"], ['を', 'に']])
+    mistake_set = np.array([["が", "は"],["から", "より"]])
     target_pattern = mistake_set[np.any(mistake_set == ppp, axis = 1)]
     if len(target_pattern) > 0:
         the_other = target_pattern[np.where(target_pattern != ppp)]
@@ -116,18 +120,34 @@ def mistake_ppp(ppp):
 
 m = MeCab.Tagger()
 
-text = "耳の外側と内側のノイズをマイクロフォンが検知。その音と釣り合うアンチノイズ機能が、あなたが聞く前にノイズを消し去ります。"
-text += "周囲の様子を聞いて対応したい時は、外部音取り込みモードに切り替えましょう。感圧センサーを長押しするだけです。"
+#text = "耳の外側と内側のノイズをマイクロフォンが検知。その音と釣り合うアンチノイズ機能が、あなたが聞く前にノイズを消し去ります。"
+#text += "周囲の様子を聞いて対応したい時は、外部音取り込みモードに切り替えましょう。感圧センサーを長押しするだけです。"
+#text = "Amazonで欲しいものを探しまわっていたら、最近は怪しい商品の日本語説明文のレベルアップがしてきたなと感じまして、日本語難しいのに勉強熱心だなと感心していたところです。そんな中、まだ発展途上だった日本語説明文が恋しくなったので自分でそういう文章を作ってみようと思いました。"
+text = "このプログラム自体は役に立ちませんが、弱い日本語の特徴を掴むことは今後の日本語教育に有用ではと思いました。日本語を勉強中の外国人たちが書いた日本語の文章の傾向を学習しクラスタリングすることで間違えやすい日本語のパターンが明らかになって、そのそれぞれにあった日本語教育カリキュラムが作れたりしそうですね。"
+'''
+text = "包み込むようなサウンドを生み出すアクティブノイズキャンセリング"\
+"周囲の音が聞こえて、今起きていることがわかる外部音取り込みモード"\
+"装着感をカスタマイズできるように、ソフトな先細のシリコーン製イヤーチップを3つのサイズで用意"\
+"耐汗耐水性能"\
+"アダプティブイコライゼーションで音楽を耳の形に合わせて自動的に調節"\
+"すべてのApple製デバイスで簡単に設定"\
+"「Hey Siri」と声をかけるだけで瞬時にSiriへアクセス"\
+"Wireless Charging Caseを使えば、バッテリー駆動時間は24時間以上"\
+'''
+
+
+
+
 node = m.parseToNode(text)
 
 word_list = []
 basic_list = []
 pos_list = []
 word_idx = 0 
+prog_bar = "▪️"
 
 while node:
     word = node.surface
-    print(node.feature.split(","))
     pos = node.feature.split(",")[0]
     conj = node.feature.split(",")[5]
     basic = node.feature.split(",")[6]
@@ -148,14 +168,15 @@ while node:
     # 変換パターン③：同音類義語変換
     elif pos in ("動詞", "形容詞", "名詞"):
         xfmd_basic = basic
-        if basic != "*":
+        if basic in ("*", "する"):
+            xfmd_word = word
+        else:
             synonym_basic = choose_synonym(basic)
             if pos in ("動詞", "形容詞"):
-                xfmd_word = transform(synonym_basic, conj)
+                if basic != "する":
+                    xfmd_word = transform(synonym_basic, conj)
             else:
                 xfmd_word = synonym_basic
-        else:
-            xfmd_word = word
     # 変換なし
     else:
         xfmd_basic = basic
@@ -164,20 +185,16 @@ while node:
     basic_list.append(xfmd_basic)
     word_list.append(xfmd_word)
     word_idx += 1;
+    prog_per = f"{'{:.1f}'.format(2*word_idx/len(text)*100)} %: "
+    prog_bar += "▪️"
+    print(f"{prog_per}:{prog_bar}")
     node = node.next
 
 
-#print(pos_list)
-#print(basic_list)
-print(word_list)
 xfmd_word_str = u"".join(word_list)
 print(text)
-print("===>")
+print("|")
+print("|")
+print("↓")
 print(xfmd_word_str)
 
-#basic = "聞く"
-#print(analyze_pos(basic))
-#synonym_basic = choose_synonym(basic)
-#print(synonym_basic)
-#xfmd_word = transform(synonym_basic, conj)
-#print(xfmd_word)
